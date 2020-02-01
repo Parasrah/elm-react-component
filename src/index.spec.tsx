dfe14089bb50f81c1a1d9dca2754ae9bdc9c7be9
parasrah <git@parasrah.com>
2020-01-31 23:28:10 -0700

    setting up test cases

--- src/index.spec.tsx
+++ src/index.spec.tsx
@@ -11,25 +11,102 @@ import createMock from './elm.mock'
 /* configure({ adapter: new Adapter() }) */
 
 describe('component', () => {
-  describe('with no props', () => {
-
-    beforeEach(() => {
+  describe('instantiation', () => {
+    describe('with explicit path', () => {
+      it('provides meaningful error when match does not exist')
+      it('successfully resolves path when match exists')
     })
 
-    it('renders successfully', () => {
-      notImplemented()
+    describe('with implicit path', () => {
+      it('provides meaningful error when multiple modules exist')
+      it('resolves to default when only a single module exists')
+    })
+  })
+
+  describe('with no props', () => {
+    describe('on mount', () => {
+      it('renders successfully')
+    })
+
+    describe('on unmount', () => {
+      it('does nothing')
     })
   })
 
   describe('with listener props', () => {
+    describe('on mount', () => {
+      it('renders successfully')
+      it('receives data from elm')
+    })
 
+    describe('on update', () => {
+      describe('no listener passed in', () => {
+        it('unsubscribes old listener')
+      })
+
+      describe('same listener passed in', () => {
+        it('does not unsubscribe listener')
+        it('does not resubscribe listener')
+      })
+
+      describe('new listener passed in', () => {
+        it('unsubscribes old listener')
+        it('subscribes new listener')
+      })
+    })
+
+    describe('on unmount', () => {
+      it('unsubscribes listeners')
+    })
   })
 
   describe('with data props', () => {
+    describe('on mount', () => {
+      describe('primitive data passed in', () => {
+        it('forwards data to port')
+      })
 
+      describe('complex data passed in', () => {
+        it('forwards data to port')
+      })
+
+      describe('undefined data passed in', () => {
+        it('forwards undefined to port')
+      })
+
+      describe('null data passed in', () => {
+        it('forwards null to port')
+      })
+    })
+
+    describe('on update', () => {
+      describe('primitive data passed in', () => {
+        it('forwards data to port')
+      })
+
+      describe('complex data passed in', () => {
+        it('forwards data to port')
+      })
+
+      describe('new data passed in', () => {
+        it('forwards data to port')
+      })
+
+      describe('same data passed in', () => {
+        it('forwards data to port')
+      })
+
+      describe('undefined data passed in', () => {
+        it('forwards undefined to port')
+      })
+
+      describe('null data passed in', () => {
+        it('forwards null to port')
+      })
+    })
+
+    describe('on unmount', () => {
+      it('does nothing')
+    })
   })
 })
-
-function notImplemented () {
-  throw new Error('not implemented')
-}
