47f94021ba5dade98a2bddede73b76200502de4d
parasrah <git@parasrah.com>
2020-02-01 22:54:20 -0700

    adding coverage requirements

--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,13 @@ module.exports = {
   transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx)$'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 95,
+      lines: 90,
+    },
+  },
   testURL: 'http://localhost',
   rootDir: __dirname,
   watchPlugins: [
